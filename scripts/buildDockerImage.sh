a8b52493e03c3768ca81b44c8b511dd7efa61ace
Nika Perfilova <gmees2016@gmail.com>
2021-10-31 20:42:15 +0700

    ci: add creating an entry in tracker issue about build success

--- scripts/buildDockerImage.sh
+++ scripts/buildDockerImage.sh
@@ -5,8 +5,9 @@ LOG_PATH=./logs/build.txt
 docker pull ubuntu
 
 VERSION=$(git describe --abbrev=0 --tags)
+BUILD_NAME="app:$VERSION"
 
-docker build -t app:"$VERSION" .
+docker build -t "$BUILD_NAME" .
 
 if [ "$?" != 0 ]; then
   echo "Во время сборки произошла ошибка" >> "$LOG_PATH"
@@ -15,3 +16,21 @@ fi
 
 echo "Docker образ собран" >> "$LOG_PATH"
 docker images app:"$VERSION" >> "$LOG_PATH"
+
+# seach an existing task for current version
+FOUND_TASKS=$(curl  -H "Authorization: OAuth $OAUTH_TOKEN" -H "X-Org-ID: $X_ORG_ID" -H 'Content-Type: application/json' --data '{"filter":{"queue": "TMP", "unique": "'"$REPO"':'"$VERSION"'"}}' https://api.tracker.yandex.net/v2/issues/_search)
+
+if [ "$FOUND_TASKS" = "[]" ]; then
+  echo "Задача для версии $VERSION не была найдена" >> $LOG_PATH
+  exit 1
+fi
+
+TASK_ID=$(echo "$FOUND_TASKS" | python3 -c "import sys, json; print(json.load(sys.stdin)[0]['id'])");
+echo "Найдена задача $TASK_ID для версии $VERSION" >> $LOG_PATH
+
+curl -H "Authorization: OAuth $OAUTH_TOKEN" -H "X-Org-ID: $X_ORG_ID" -H 'Content-Type: application/json' --data '{"text": "Собран контейнер '"$BUILD_NAME"'"}' https://api.tracker.yandex.net/v2/issues/"$TASK_ID"/comments
+
+if [ "$?" != 0 ]; then
+  echo "Не удалось оставить комментарий в задаче" >> $LOG_PATH
+  exit 1
+fi
