716b84136f25bf6ea73efa497ae27bd964766b04
Nika Perfilova <gmees2016@gmail.com>
2021-10-31 20:03:18 +0700

    ci: try another way to install node in docker container

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,12 @@
 FROM ubuntu:20.04
 WORKDIR /app
 COPY . .
-RUN sudo apt update
-RUN sudo apt install nodejs
-RUN sudo apt install npm
+RUN apt-get update && apt-get install -y \
+    software-properties-common \
+    npm
+RUN npm install npm@latest -g && \
+    npm install n -g && \
+    n latest
 RUN npm ci
 RUN npm run build
 CMD npm start
--- scripts/buildDockerImage.sh
+++ scripts/buildDockerImage.sh
@@ -4,11 +4,14 @@ LOG_PATH=./logs/build.txt
 
 docker pull ubuntu
 
-echo "$VERSION" >> "$LOG_PATH"
-
 VERSION=$(git describe --abbrev=0 --tags)
 
 docker build -t app:"$VERSION" .
+
+if [ "$?" != 0 ]; then
+  echo "Во время сборки произошла ошибка" >> "$LOG_PATH"
+  exit 1
+fi
 
 echo "Docker образ собран" >> "$LOG_PATH"
 docker images app:"$VERSION" >> "$LOG_PATH"
