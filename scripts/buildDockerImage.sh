c60acae97cbf2bd98fc8df600f9a29dd0d63d3b8
Nika Perfilova <gmees2016@gmail.com>
2021-10-31 23:51:57 +0700

    ci: remove unnecessary requests to tracker

--- scripts/buildDockerImage.sh
+++ scripts/buildDockerImage.sh
@@ -17,27 +17,13 @@ fi
 echo "Docker образ $BUILD_NAME собран" >> "$LOG_PATH"
 docker images app:"$VERSION" >> "$LOG_PATH"
 
-# seach an existing task for current version
-echo "Запрос на поиск задач в трекере по полю unique=$REPO:$VERSION" >> $LOG_PATH
-
-FOUND_TASKS=$(curl  -H "Authorization: OAuth $OAUTH_TOKEN" -H "X-Org-ID: $X_ORG_ID" -H 'Content-Type: application/json' --data '{"filter":{"queue": "TMP", "unique": "'"$REPO"':'"$VERSION"'"}}' https://api.tracker.yandex.net/v2/issues/_search)
-
-if [ "$FOUND_TASKS" = "[]" ]; then
-  echo "Задача для версии $VERSION не была найдена" >> $LOG_PATH
-  exit 1
-fi
-
-echo "Найдена задача $FOUND_TASKS" >> $LOG_PATH
-
-# TASK_KEY=$(echo "$FOUND_TASKS" | python3 -c "import sys, json; print(json.load(sys.stdin)[0]['key'])");
 TASK_KEY=$(node -e "const issue = require('./releaseIssue.json'); console.log(issue['key'])");
 
-echo "Найдена задача $TASK_KEY" >> $LOG_PATH
+echo "Отправка запроса на создание комментария в задаче $TASK_KEY" >> $LOG_PATH
 
 RESPONSE=$(curl -H "Authorization: OAuth $OAUTH_TOKEN" -H "X-Org-ID: $X_ORG_ID" -H 'Content-Type: application/json' --data '{"text": "Собран контейнер '"$BUILD_NAME"'"}' https://api.tracker.yandex.net/v2/issues/"$TASK_KEY"/comments)
 
 HAS_ERRORS=$(echo "$RESPONSE" | python3 -c "import sys, json; print(hasattr(json.load(sys.stdin), 'errors'))");
-
 
 if [ "$HAS_ERRORS" = "true" ]; then
   echo "Не удалось оставить комментарий в задаче" >> $LOG_PATH
--- scripts/runTests.sh
+++ scripts/runTests.sh
@@ -11,22 +11,11 @@ fi
 
 TESTS_RESULT=$(node -e "const result = require('./testResults.json'); console.log(result.success ? 'Success' : 'Fail')")
 
-# seach an existing task for current version
-echo "Запрос на поиск задач в трекере по полю unique=$REPO:$VERSION" >> $LOG_FILE
-
-FOUND_TASKS=$(curl  -H "Authorization: OAuth $OAUTH_TOKEN" -H "X-Org-ID: $X_ORG_ID" -H 'Content-Type: application/json' --data '{"filter":{"queue": "TMP", "unique": "'"$REPO"':'"$VERSION"'"}}' https://api.tracker.yandex.net/v2/issues/_search)
-
-if [ "$FOUND_TASKS" = "[]" ]; then
-  echo "Задача для версии $VERSION не была найдена" >> $LOG_FILE
-  exit 1
-fi
-
-# TASK_KEY=$(echo "$FOUND_TASKS" | python3 -c "import sys, json; print(json.load(sys.stdin)[0]['key'])")
 TASK_KEY=$(node -e "const issue = require('./releaseIssue.json'); console.log(issue['key'])")
 
-echo "Найдена задача $TASK_KEY" >> $LOG_FILE
+echo "Отпрака запроса на создание комментария в задаче $TASK_KEY" >> $LOG_FILE
 
-RESPONSE=$(curl -H "Authorization: OAuth $OAUTH_TOKEN" -H "X-Org-ID: $X_ORG_ID" -H 'Content-Type: application/json' --data '{"text": "Результат запуска тестов: '"$TESTS_RESULT"'"}' https://api.tracker.yandex.net/v2/issues/"$TASK_KEY"/comments)
+RESPONSE=$(curl -H "Authorization: OAuth $OAUTH_TOKEN" -H "X-Org-ID: $X_ORG_ID" -H 'Content-Type: application/json' --data '{"text": "Результат запуска тестов: '"$TESTS_RESULT"', testResults.json"}' https://api.tracker.yandex.net/v2/issues/"$TASK_KEY"/comments)
 
 HAS_ERRORS=$(echo "$RESPONSE" | python3 -c "import sys, json; print(hasattr(json.load(sys.stdin), 'errors'))");
 
